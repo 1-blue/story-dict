ae1b2ea8875f888944c234046933eebbdb86e861
1-blue <1-blue98@naver.com>
2025-10-26 12:39:32 +0900

    🚚: 절대경로 변경 ( @ → # )

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,7 +1,7 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
-import "@/global.css";
+import "#/global.css";
 
 export const unstable_settings = {
   anchor: "(tabs)",
