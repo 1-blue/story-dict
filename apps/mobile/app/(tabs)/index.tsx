ec3abe8209f9fc8738b66479e310ffeb25bab9f3
1-blue <1-blue98@naver.com>
2025-10-27 16:10:59 +0900

    💄: Reusables 세팅 및 적용

--- apps/mobile/app/(tabs)/index.tsx
+++ apps/mobile/app/(tabs)/index.tsx
@@ -1,13 +1,17 @@
-import { Text, View } from "react-native";
+import { Button } from "#/src/components/ui/button";
+import { Text } from "#/src/components/ui/text";
+import { View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const HomeScreen = () => {
   return (
     <SafeAreaView>
       <View>
-        <Text className="text-red-500 font-bold bg-indigo-500">
-          메인 페이지
-        </Text>
+        <Button variant="default">
+          <Text variant="default">내가 만든 버튼</Text>
+        </Button>
+
+        <Text className="text-red-900">무지</Text>
       </View>
     </SafeAreaView>
   );
--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,23 +1,31 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
-import "#/global.css";
+import "../src/css/global.css";
+import { PortalHost } from "@rn-primitives/portal";
+import { ThemeProvider } from "@react-navigation/native";
+import { NAV_THEME } from "#/src/libs/theme";
+import { useColorScheme } from "react-native";
 
 export const unstable_settings = {
   anchor: "(tabs)",
 };
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
   return (
-    <>
+    <ThemeProvider value={NAV_THEME[colorScheme ?? "light"]}>
+      <StatusBar style={colorScheme === "dark" ? "light" : "dark"} />
+
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen
           name="modal"
           options={{ presentation: "modal", title: "Modal" }}
         />
+        <PortalHost />
       </Stack>
-      <StatusBar style="auto" />
-    </>
+    </ThemeProvider>
   );
 }
